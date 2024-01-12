972c959fb37f539e12fa2c3e1abf84dca1a74d05
Rox221 <118830247+Rox221@users.noreply.github.com>
2024-01-12 13:13:08 +0630

    dessert.js

--- dessert.js
+++ dessert.js
@@ -23,26 +23,3 @@ imageGrid.addEventListener('click', function(event) {
     addToCartBtn.classList.toggle('hidden');
   }
 });
-
-var asyncImages = document.querySelectorAll('.async-image');
-
-function loadImageAsync(img) {
-    var imagePath = img.dataset.src;
-    console.log('Attempting to load image: ' + imagePath);
-    
-    var tempImg = new Image();
-    tempImg.src = imagePath;
-    tempImg.onload = function() {
-        img.src = imagePath;
-        console.log('Image loaded successfully: ' + imagePath);
-    };
-    tempImg.onerror = function() {
-        console.error('Error loading image: ' + imagePath);
-    };
-}
-
-// Call the function to load images asynchronously for each img tag
-asyncImages.forEach(function(img) {
-    loadImageAsync(img);
-});
-      
