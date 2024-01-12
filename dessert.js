8d587787cbab37f95e9c81d3132ea6f1c2cab34f
Rox221 <118830247+Rox221@users.noreply.github.com>
2024-01-12 13:08:55 +0630

    dessert.js

--- dessert.js
+++ dessert.js
@@ -26,17 +26,20 @@ imageGrid.addEventListener('click', function(event) {
 
 var asyncImages = document.querySelectorAll('.async-image');
 
-        function loadImageAsync(img) {
-            var imagePath = img.dataset.src;
-            var tempImg = new Image();
-            tempImg.src = imagePath;
-            tempImg.onload = function() {
-                img.src = imagePath;
-                
-            };
-        }
+function loadImageAsync(img) {
+    var imagePath = img.dataset.src;
+    var tempImg = new Image();
+    tempImg.src = imagePath;
+    tempImg.onload = function() {
+        img.src = imagePath;
+        // No need for console.log if you don't want it
+    };
+    tempImg.onerror = function() {
+        console.error('Error loading image: ' + imagePath);
+    };
+}
 
-        // Call the function to load images asynchronously for each img tag
-        asyncImages.forEach(function(img) {
-            loadImageAsync(img);
-        });
+// Call the function to load images asynchronously for each img tag
+asyncImages.forEach(function(img) {
+    loadImageAsync(img);
+});
